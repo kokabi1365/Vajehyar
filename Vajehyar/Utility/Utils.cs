c5956de6c0e734382de0b896b0840491a4e5836e
Ayub <kokabi1365@gmail.com>
2019-06-21 11:26:05 +0430

    Improve UI

--- Vajehyar/Utility/Utils.cs
+++ Vajehyar/Utility/Utils.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace Vajehyar.Utility
@@ -20,6 +21,39 @@ namespace Vajehyar.Utility
             }).ContinueWith(
                 t => 
             );*/
+        }
+
+
+    }
+
+    public static class KeyboardFocus
+    {
+        public static readonly DependencyProperty OnProperty;
+
+        public static void SetOn(UIElement element, FrameworkElement value)
+        {
+            element.SetValue(OnProperty, value);
+        }
+
+        public static FrameworkElement GetOn(UIElement element)
+        {
+            return (FrameworkElement)element.GetValue(OnProperty);
+        }
+
+        static KeyboardFocus()
+        {
+            OnProperty = DependencyProperty.RegisterAttached("On", typeof(FrameworkElement), typeof(KeyboardFocus), new PropertyMetadata(OnSetCallback));
+        }
+
+        private static void OnSetCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var frameworkElement = (FrameworkElement)dependencyObject;
+            var target = GetOn(frameworkElement);
+
+            if (target == null)
+                return;
+
+            frameworkElement.Loaded += (s, e) => Keyboard.Focus(target);
         }
     }
 }
