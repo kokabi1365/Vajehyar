df80ef7850d708d49e1233de07858117694e1a0a
Ayub <kokabi1365@gmail.com>
2019-12-23 21:16:30 +0330

    Remove diacritics while searching

--- Vajehdan/Windows/MainWindow.xaml.cs
+++ Vajehdan/Windows/MainWindow.xaml.cs
@@ -115,7 +115,7 @@ namespace Vajehdan.Windows
                 case false:
                     for (int i = 0; i < words.Count; i++)
                     {
-                        if (words[i].RemoveDiacritics() == _filterString)
+                        if (words[i].RemoveDiacritics() == _filterString.RemoveDiacritics())
                         {
                             return true;
                         }
@@ -126,7 +126,7 @@ namespace Vajehdan.Windows
                 case true:
                     for (int i = 0; i < words.Count; i++)
                     {
-                        if (words[i].RemoveDiacritics().Contains(_filterString))
+                        if (words[i].RemoveDiacritics().Contains(_filterString.RemoveDiacritics()))
                         {
                             return true;
                         }
@@ -146,7 +146,7 @@ namespace Vajehdan.Windows
 
             for (int i = 0; i < words.Count; i++)
             {
-                if (words[i].RemoveDiacritics().Contains(_filterString))
+                if (words[i].RemoveDiacritics().Contains(_filterString.RemoveDiacritics()))
                 {
                     return true;
                 }
