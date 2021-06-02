76ea2a68e6c7d791a4dd615a645bc36348e95c50
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2021-06-02 18:27:12 +0430

    افزایشِ شمارهٔ نسخه

--- src/Vajehdan/Properties/AssemblyInfo.cs
+++ src/Vajehdan/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("Vajehdan")]
 [assembly: AssemblyProduct("Vajehdan")]
 [assembly: AssemblyDescription("A persian dictionary")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.0.1.0")]
+[assembly: AssemblyFileVersion("5.0.1.0")]
